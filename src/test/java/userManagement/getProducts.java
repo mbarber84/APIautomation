7d1a95897af8406ea1c1dba6de0f620e3cae6b10
MathewBarber <mbarber_84@hotmail.co.uk>
2026-02-17 10:24:59 +0000

    validation Contains, HasSize, HasItems commit

--- src/test/java/userManagement/getProducts.java
+++ src/test/java/userManagement/getProducts.java
@@ -3,6 +3,9 @@ package userManagement;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
+
+import java.util.*;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -87,6 +90,43 @@ public class getProducts {
         assertThat(response.jsonPath().getList(""), hasSize(3));
     }
 
+//    @Test
+//    public void validateListContainsInOrder() {
+//        // Set base URI for the API
+//        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+//
+//        // Send a GET request and store the response in a variable
+//        Response response = given()
+//                .when()
+//                .get("/comments?postId=1")
+//                .then()
+//                .extract()
+//                .response();
+//
+//        // Use Hamcrest to check that the response body contains specific items in a specific order
+//        List<String> expectedEmails = Arrays.asList("Eliseo@gardner.biz", "Jayne_Kuhic@sydney.com", "Nikita@garfield.biz","Lew@alysha.tv","Hayden@althea.biz");
+//        assertThat(response.jsonPath().getList("email"), contains(expectedEmails.toArray(new String[0])));
+//    }
+
+    @Test
+    public void validateListContainsInOrder() {
+        // Set base URI for your API
+        RestAssured.baseURI = "http://localhost:2002";
+
+        // Send a GET request and store the response
+        Response response = given()
+                .when()
+                .get("/api/products")
+                .then()
+                .extract()
+                .response();
+
+        // Expected product names in exact order
+        List<String> expectedNames = Arrays.asList("iPad", "iPhone X", "Google Tablet");
+
+        // Assert that the "name" field contains the expected names in the same order
+        assertThat(response.jsonPath().getList("name"), contains(expectedNames.toArray(new String[0])));
+    }
 
 
 
